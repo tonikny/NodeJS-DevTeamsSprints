47d105c6521c203b13691b854b043bc8f1688337
Ferran Escolà <escolasarabia@gmail.com>
2022-01-24 10:29:18 +0100

    app funcional, tascaService actualitzat

--- app/services/TascaService.js
+++ app/services/TascaService.js
@@ -9,19 +9,22 @@ class TascaService {
   }
 
   afegirTasca(dades) {
+    console.log('afegir tasca')
     const tasca = new Tasca(dades);
     return tasca.afegir();
   }
 
   veureTasca(id) {
-    //const tasca = new Tasca();
+    const tasca = new Tasca();
     const tascaTrobada = this.cercarTasca(id);
     console.log(`La tasca es ${ tascaTrobada }`);
   }
 
   cercarTasca(id) {
     const tasca = new Tasca();
-    return tasca.obtenir(id);
+    console.log(`cercar tasca${tasca.obtenir(id)}`)
+    //return tasca.obtenir(id);
+
   }
 
   async actualitzarTasca(dades) {
@@ -35,13 +38,18 @@ class TascaService {
   }
 
   async llistarTasques() {
+    console.log('llistartasques')
     const tasca = new Tasca();
+    const tasques = tasca.llistar();
+    for (let i = 0; i < tasques.length; i ++) {
+      console.log(`tasca : ${ tasques[i]}`)
+    }
+    //tasca.llistar();
     return await tasca.database.llistar();
     return await tasca.llistar();
   }
 
 }
-
 
 
 module.exports = { TascaService };
