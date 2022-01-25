2c3da162cb1270b41c19928b3853f169febc3bab
Abdel Chicas <amchicas@gmail.com>
2022-01-25 11:04:17 +0100

    canvi de nom variable database a model

--- app/app.js
+++ app/app.js
@@ -3,8 +3,8 @@ const app = require('./controller/menu.js');
 const { TascaService } = require('./services/TascaService');
 const  { Tasca }  = require('./models/TascaJson');
 
-const database = new Tasca();
-const tascaService = new TascaService(database);
+const model = new Tasca();
+const tascaService = new TascaService(model);
 
 app.start(tascaService);
 
