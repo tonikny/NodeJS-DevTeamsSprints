4e5279f45900cb2862c1644cca28ea537e409580
Ferran Escolà <escolasarabia@gmail.com>
2022-02-07 12:06:20 +0100

    fix mdb

--- app/models/TascaMDB.js
+++ app/models/TascaMDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_CONN)
+mongoose.connect(process.env.MONGO_CONN+'?serverSelectionTimeoutMS=5000')
     .then(() => console.log('connected to mongodb'))
     .catch(() =>{ 
         console.log("Conexió a MongoDB ha trigat massa")
